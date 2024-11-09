fd964dabc7dd58d329916a3bfd23414ccf53db17
Lutfi Fajar <60343449+severusDude@users.noreply.github.com>
2024-11-09 22:36:08 +0700

    feat(user): add custom panel shield trait
    panel shield trait only allow the access of panels
    removal of panel user role auto assignment on new user creation

---
+++ app/Traits/HasPanelShield.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Traits;
+
+use BezhanSalleh\FilamentShield\Support\Utils;
+use Filament\Panel;
+
+trait HasPanelShield
+{
+    public static function bootHasPanelShield()
+    {
+        if (Utils::isPanelUserRoleEnabled()) {
+
+            Utils::createPanelUserRole();
+
+            // static::created(fn($user) => $user->assignRole(Utils::getPanelUserRoleName()));
+
+            // static::deleting(fn($user) => $user->removeRole(Utils::getPanelUserRoleName()));
+        }
+    }
+
+    public function canAccessPanel(Panel $panel): bool
+    {
+        return $this->hasRole(Utils::getSuperAdminName()) || $this->hasRole(Utils::getPanelUserRoleName());
+    }
+}
