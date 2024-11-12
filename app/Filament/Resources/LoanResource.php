3ce2233b63d6980fccf6f3964834827c76558316
Lutfi Fajar <60343449+severusDude@users.noreply.github.com>
2024-11-12 12:38:13 +0700

    feat(loan): add loan resource

---
+++ app/Filament/Resources/LoanResource.php
@@ -0,0 +1,81 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use App\Filament\Resources\LoanResource\Pages;
+use App\Filament\Resources\LoanResource\RelationManagers;
+use App\Models\Loan;
+use Filament\Forms;
+use Filament\Forms\Form;
+use Filament\Resources\Resource;
+use Filament\Tables;
+use Filament\Tables\Table;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+
+class LoanResource extends Resource
+{
+    protected static ?string $model = Loan::class;
+
+    protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                Forms\Components\TextInput::make('')
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                Tables\Columns\TextColumn::make('id')
+                    ->label('ID'),
+                Tables\Columns\TextColumn::make('lab.name')
+                    ->label('Nama Lab'),
+                Tables\Columns\TextColumn::make('created_by.name')
+                    ->label('Peminjam'),
+                Tables\Columns\TextColumn::make('subject.name'),
+                Tables\Columns\TextColumn::make('effect_date')
+                    ->dateTime(),
+                Tables\Columns\TextColumn::make('end_date')
+                    ->dateTime(),
+                Tables\Columns\IconColumn::make('repeat')
+                    ->boolean()
+                    ->label('Berulang'),
+                Tables\Columns\TextColumn::make('approval_by.name'),
+                Tables\Columns\TextColumn::make('updated_at')
+                    ->dateTime()
+                    ->label('Last Modified'),
+            ])
+            ->filters([
+                //
+            ])
+            ->actions([
+                Tables\Actions\EditAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\BulkActionGroup::make([
+                    Tables\Actions\DeleteBulkAction::make(),
+                ]),
+            ]);
+    }
+
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListLoans::route('/'),
+            'create' => Pages\CreateLoan::route('/create'),
+            'edit' => Pages\EditLoan::route('/{record}/edit'),
+        ];
+    }
+}
