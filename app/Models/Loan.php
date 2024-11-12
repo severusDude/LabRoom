e263d3334bb4447d75cf7e4551f5484e344ff12c
Lutfi Fajar <60343449+severusDude@users.noreply.github.com>
2024-11-12 13:46:07 +0700

    feat(loan): define loan guard
    all attributes all fillable except for id, created_at, updated_at

--- app/Models/Loan.php
+++ app/Models/Loan.php
@@ -9,6 +9,12 @@ class Loan extends Model
 {
     use HasFactory;
 
+    protected $guard = [
+        'id',
+        'created_at',
+        'updated_at',
+    ];
+
     public function lab()
     {
         return $this->belongsTo(Lab::class);
