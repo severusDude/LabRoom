8c055d14785a018ae2621672f92653199ea044e3
Lutfi Fajar <60343449+severusDude@users.noreply.github.com>
2024-11-20 15:53:10 +0700

    chore(loan): add loan seeder and factory

---
+++ database/factories/LoanFactory.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Lab;
+use App\Models\User;
+use App\Models\Subject;
+use Illuminate\Support\Carbon;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Loan>
+ */
+class LoanFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        $effect_date = fake()->dateTimeBetween(now(), now()->endOfMonth());
+        $end_date = Carbon::instance($effect_date)->addHours(rand(1, 4));
+        $created_by = User::query()->whereHas('roles', function ($query) {
+            $query->where('name', 'standard');
+        })->get();
+
+        return [
+            'lab_id' => Lab::all()->random()->id,
+            'created_by' => $created_by->random()->id,
+            'subject_id' => Subject::all()->random()->id,
+            'effect_date' => $effect_date,
+            'end_date' => $end_date,
+            'is_repeat' => false,
+        ];
+    }
+}
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -18,6 +18,7 @@ class DatabaseSeeder extends Seeder
             UserSeeder::class,
             LabSeeder::class,
             SubjectSeeder::class,
+            LoanSeeder::class,
         ]);
     }
 }
