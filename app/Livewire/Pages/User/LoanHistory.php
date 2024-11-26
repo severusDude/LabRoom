dcf310000ea2265d5bf9cc7e189127ef386e457a
raihannurhidayat <grandrx041@gmail.com>
2024-11-26 11:50:45 +0700

    feat: add responsif history loan

--- app/Livewire/Pages/User/LoanHistory.php
+++ app/Livewire/Pages/User/LoanHistory.php
@@ -8,17 +8,22 @@ use Livewire\Component;
 
 class LoanHistory extends Component
 {
-    public $historyLoan;
+    // public $historyLoan;
     public $userId;
 
     public function mount()
     {
         $this->userId = Auth::id();
-        $this->historyLoan = Loan::with("lab", 'subject', "approval.user")->where("created_by", $this->userId)->orderBy("created_at", "desc")->get()->all();
+        // $this->historyLoan = Loan::with("lab", 'subject', "approval.user")->where("created_by", $this->userId)->orderBy("created_at", "desc")->get()->all();
     }
 
     public function render()
     {
-        return view('livewire.pages.user.loan-history');
+        return view('livewire.pages.user.loan-history', [
+            'historyLoan' => Loan::with("lab", 'subject', "approval.user")
+                ->where("created_by", $this->userId)
+                ->orderBy("created_at", "desc")
+                ->paginate(5)
+        ]);
     }
 }
--- resources/views/livewire/pages/user/loan-history.blade.php
+++ resources/views/livewire/pages/user/loan-history.blade.php
@@ -1,4 +1,4 @@
-<div class="mt-4">
+<div class="lg:pt-0 pt-20">
     <h1 class="text-3xl font-semibold tracking-tighter mb-4 text-slate-700">Riwayat Peminjaman</h1>
     <div>
         {{-- CARD --}}
@@ -6,6 +6,7 @@
             @foreach ($historyLoan as $data)
                 <livewire:components.card-history :history="$data" />
             @endforeach
+            {{ $historyLoan->links() }}
         </div>
     </div>
 </div>
