ce61174ef7a705f486894810b6d8857afbecf327
severusDude <fajarsaladin05@gmail.com>
2024-11-27 15:01:15 +0700

    feat(card lab): update card lab ui

--- resources/views/livewire/pages/user/home.blade.php
+++ resources/views/livewire/pages/user/home.blade.php
@@ -1,6 +1,6 @@
-<div class="lg:px-4 lg:py-0 py-20 px-0">
-    <h1 class="text-2xl lg:text-3xl font-semibold tracking-tighter mb-4">Selamat Datang 👏</h1>
-    <div class="flex gap-3 flex-wrap flex-1">
+<div class="px-0 py-20 lg:px-4 lg:py-0">
+    <h1 class="mb-4 text-2xl font-semibold tracking-tighter lg:text-3xl">Selamat Datang 👏</h1>
+    <div class="flex flex-wrap flex-1 gap-4">
         @foreach ($labs as $index => $lab)
             <livewire:components.card-lab :lab="$lab" :image="$images[$index]" />
         @endforeach
