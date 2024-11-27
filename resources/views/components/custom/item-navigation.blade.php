2b321ccd2a99faf461e69daa0797003221520a73
severusDude <fajarsaladin05@gmail.com>
2024-11-27 15:15:58 +0700

    feat(navbar): update navbar ui

--- resources/views/components/custom/item-navigation.blade.php
+++ resources/views/components/custom/item-navigation.blade.php
@@ -1,7 +1,7 @@
 @props(['icon', 'active' => false, 'href'])
 
 <a href="{{ $href }}"
-    class="flex items-center gap-2 px-2 py-1 {{ $active ? 'border border-white rounded ' : '' }} cursor-pointer hover:bg-[#2BA8D8] rounded transition-all ease-in-out">
-    <x-dynamic-component :component="$icon" class="h-5 w-5 text-white" />
-    <p class="text-lg">{{ $slot }}</p>
+    class="flex items-center gap-2 px-2 py-1 {{ $active ? 'border border-white rounded ' : '' }} cursor-pointer hover:bg-white hover:text-primary-800 rounded-lg group">
+    <x-dynamic-component :component="$icon" class="w-5 h-5 text-white group-hover:text-primary-800" />
+    <p class="text-lg group-hover:text-primary-800">{{ $slot }}</p>
 </a>
--- resources/views/livewire/components/navigation-desktop.blade.php
+++ resources/views/livewire/components/navigation-desktop.blade.php
@@ -1,8 +1,8 @@
-<nav class="hidden w-[314px] min-h-screen bg-[#035B89] px-[50px] pt-[25px] lg:flex flex-col fixed">
+<nav class="hidden w-[314px] min-h-screen bg-primary-800 px-[50px] pt-[25px] lg:flex flex-col fixed">
     <div>
         <h1 class="text-[40px] text-white mx-auto">LAB-ROOM</h1>
     </div>
-    <div class="text-white mt-4">
+    <div class="mt-4 text-white">
         <div class="space-y-2">
             <x-custom.item-navigation :icon="'heroicon-o-home'" :active="request()->routeIs('user.home')" href="{{ route('user.home') }}">
                 Beranda
@@ -16,8 +16,8 @@
         </div>
     </div>
     <div
-        class="flex items-center gap-2 px-2 py-1 mt-auto mb-12 cursor-pointer hover:bg-[#2BA8D8] rounded transition-all ease-in-out">
-        <x-heroicon-o-arrow-left-end-on-rectangle class="h-5 w-5 text-white" />
-        <button wire:click="logout" class="text-lg text-white">Logout</button>
+        class="flex items-center gap-2 px-2 py-1 mt-auto mb-12 rounded cursor-pointer hover:bg-white hover:text-primary-800 group">
+        <x-heroicon-o-arrow-left-end-on-rectangle class="w-5 h-5 text-white group-hover:text-primary-800" />
+        <button wire:click="logout" class="text-lg text-white group-hover:text-primary-800">Logout</button>
     </div>
 </nav>
