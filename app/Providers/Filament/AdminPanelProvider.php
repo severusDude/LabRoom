b93293e48b3c862b22227cb5472d233844f2c678
raihannurhidayat <grandrx041@gmail.com>
2024-12-01 21:23:35 +0700

    feat(admin): add feature widget

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -2,6 +2,8 @@
 
 namespace App\Providers\Filament;
 
+use App\Filament\Resources\WidgetResource\Widgets\StatsAdminLoanOverview;
+use App\Filament\Resources\WidgetResource\Widgets\UrgentLoan;
 use Filament\Http\Middleware\Authenticate;
 use Filament\Http\Middleware\DisableBladeIconComponents;
 use Filament\Http\Middleware\DispatchServingFilamentEvent;
@@ -40,8 +42,10 @@ class AdminPanelProvider extends PanelProvider
             ])
             ->discoverWidgets(in: app_path('Filament/Widgets'), for: 'App\\Filament\\Widgets')
             ->widgets([
-                Widgets\AccountWidget::class,
-                Widgets\FilamentInfoWidget::class,
+                // Widgets\AccountWidget::class,
+                // Widgets\FilamentInfoWidget::class,
+                StatsAdminLoanOverview::class,
+                UrgentLoan::class
             ])
             ->middleware([
                 EncryptCookies::class,
